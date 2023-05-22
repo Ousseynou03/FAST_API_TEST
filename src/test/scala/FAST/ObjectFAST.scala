dd7ac24b408391274f9a4a7d990870b88ebd0d89
Dione <dioneousseynou03@gmail.com>
2023-05-22 17:53:47 +0100

    add bodyPart post request

--- src/test/scala/FAST/ObjectFAST.scala
+++ src/test/scala/FAST/ObjectFAST.scala
@@ -11,34 +11,27 @@ import scala.language.postfixOps
 
 object ObjectFAST {
 
-
   private val tpsPaceDefault: Int = System.getProperty("tpsPace", "1000").toInt
   private val tpsPacingProducts: Int = System.getProperty("tpsPaceProducts", tpsPaceDefault.toString).toInt
-
-
 
   private val TpsPausemin: Int = System.getProperty("tpsPause", "3").toInt
   private val TpsPausemax: Int = System.getProperty("tpsPause", "11").toInt
   private val TempoMillisecond: Int = System.getProperty("TempoMillisecond", "10").toInt
 
-
   private val NbreIterTransacDefault: Int = System.getProperty("nbIter", "3").toInt
   private val NbreIter: Int = System.getProperty("nbIterTransac", NbreIterTransacDefault.toString).toInt
 
-
-
+  //Variable aléatoire de numCaisse selon le format 001(001 -> 999)
   val numCaisse: Iterator[String] = Iterator.from(1).map(i => f"${(i-1) % 999 + 1}%03d")
 
+  //VAriable aléatoire de numSequence selon le format 001(001 -> 999)
   val numSequence: Iterator[String] = Iterator.from(1).map(i => f"${(i - 1) % 999 + 1}%03d")
 
-
   val startDateTime = LocalDateTime.parse("2023-04-03T07:00:01")
-  // Définir le format de date et heure ISO 8601
   val dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+  val dateTime: Iterator[String] = Iterator.iterate(startDateTime)(_.plusSeconds(1)).map(dateTimeFormatter.format)
 
-  // Définir l'itérateur qui incrémente la date et l'heure de 1 seconde à chaque itération
-  val dateTime: Iterator[String] = Iterator.iterate(startDateTime)(_.plusSeconds(1))
-    .map(dateTimeFormatter.format)
+  val filePath = "data/file.xml" // Chemin du fichier XML
 
 
   val scnFastApi = scenario("GET CUSTOMER FAST API")
@@ -50,9 +43,9 @@ object ObjectFAST {
       println("code_magasin :" + session("code_magasin").as[String])
       session
     }
-    .exec{session =>
-      val num_caisse : String = numCaisse.next()
-      session.set("numero_caisse",num_caisse)
+    .exec { session =>
+      val num_caisse: String = numCaisse.next()
+      session.set("numero_caisse", num_caisse)
     }
     .exec { session =>
       println("numero_caisse : " + session("numero_caisse").as[String])
@@ -74,92 +67,20 @@ object ObjectFAST {
       println("nextDateTime : " + session("nextDateTime").as[String])
       session
     }
-    .exec(
-      http(" POST FAST API")
-        .post("/ws/fast/tx6")
-        .header("Content-Type", "application/xml")
-        .header("Accept", "application/xml")
-        .asXml
-        .body(StringBody(
-          """
-            |<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-            |<POSLog MajorVersion="6"
-            |xsi:schemaLocation="http://www.nrf-arts.org/IXRetail/namespace/ POSLogV6.0.0.xsd"
-            |xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-            |xmlns="http://www.nrf-arts.org/IXRetail/namespace/">
-            |<Transaction>
-            |<BusinessUnit>
-            |<UnitID TypeCode="RetailStore">${code_magasin}</UnitID>
-            |</BusinessUnit>
-            |<Channel>
-            |<ChannelID>STORE</ChannelID>
-            |<TouchPointID>POS</TouchPointID>
-            |</Channel>
-            |<WorkstationID>${numero_caisse}</WorkstationID>
-            | <SequenceNumber>${numero_sequence}</SequenceNumber>
-            |<OperatorID>156994</OperatorID>
-            |<RetailTransaction>
-            |<LineItem EntryMethod="Scanned">
-            |<Sale ItemSubType="x:ORI3" ItemType="Stock">
-            |<ItemID Name="SKU">54437934</ItemID>
-            |<ItemID Name="Codabar">2022854437939</ItemID>
-            |<MerchandiseHierarchy Level="Group">228</MerchandiseHierarchy>
-            |<MerchandiseHierarchy Level="Department">2079</MerchandiseHierarchy>
-            |<POSIdentity>
-            |<POSItemID>3258541613075</POSItemID>
-            |<Qualifier>1</Qualifier>
-            |</POSIdentity>
-            |<ActualSalesUnitPrice>30.00</ActualSalesUnitPrice>
-            |<ExtendedAmount>30.00</ExtendedAmount>
-            |<ExtendedDiscountAmount>24.0</ExtendedDiscountAmount>
-            |<Quantity UnitOfMeasureCode="EA">1.0</Quantity>
-            |<Associate>
-            |<AssociateID>156994</AssociateID>
-            |</Associate>
-            |<Tax TaxType="VAT">
-            |<Percent>20.0</Percent>
-            |</Tax>
-            |<SellingLocation>12345</SellingLocation>
-            |</Sale>
-            |<SequenceNumber>1</SequenceNumber>
-            |</LineItem>
-            |<LineItem>
-            |<Tender TenderType="x:TTID1000CASH">
-            |<Amount>10.0</Amount>
-            |</Tender>
-            |<SequenceNumber>2</SequenceNumber>
-            |</LineItem>
-            |<LineItem>
-            |<Tender TenderType="x:TTID3030CCARD">
-            |<Amount>20.0</Amount>
-            |<Authorization HostAuthorized="false" PreAuthorizationFlag="false" VerifiedByPINFlag="false">
-            |<AuthorizationCode>0000</AuthorizationCode>
-            |<ReferenceNumber>0</ReferenceNumber>
-            |<AuthorizingTermID>780</AuthorizingTermID>
-            |<EMVDebug>
-            |<CardholderVerificationMethodResults>E</CardholderVerificationMethodResults>
-            |<TerminalVerificationResults>T</TerminalVerificationResults>
-            |</EMVDebug>
-            |</Authorization>
-            |<CreditDebit CardType="Credit" TypeCode="Visa">
-            |<IssuerIdentificationNumber>1</IssuerIdentificationNumber>
-            |<PrimaryAccountNumber>000000000000000</PrimaryAccountNumber>
-            |<ExpirationDate>2099-01</ExpirationDate>
-            |<CreditCardCompanyCode>T 0</CreditCardCompanyCode>
-            |</CreditDebit>
-            |</Tender>
-            |<SequenceNumber>3</SequenceNumber>
-            |</LineItem>
-            |<Total TotalType="TransactionGrandAmount">30.0</Total>
-            |<LoyaltyAccount>
-            |<CustomerID>{{client_id:407190620}}</CustomerID>
-            |</LoyaltyAccount>
-            |</RetailTransaction>
-            |<EndDateTime>{{date_time:${nextDateTime}}</EndDateTime>
-            |</Transaction>
-            |</POSLog>
-            |""".stripMargin)).asXml
-        .check(status is 201)
-    )
-    .pause(TpsPausemin seconds,TpsPausemax seconds)
+    .exec(session => session.set("filePath", filePath))
+    .exec(session => {
+      val xmlContenu = session("filePath").as[String]
+        .replaceAll("\\$\\{code_magasin}", session("code_magasin").as[String])
+        .replaceAll("\\$\\{numero_caisse}", session("numero_caisse").as[String])
+        .replaceAll("\\$\\{numero_sequence}", session("numero_sequence").as[String])
+        .replaceAll("\\$\\{nextDateTime}", session("nextDateTime").as[String])
+      session.set("xmlContenu", xmlContenu)
+    })
+    .exec(http("Upload POSLog XML")
+      .post("/ws/fast/tx6")
+      .basicAuth("<login_esb>", "<password>")
+      .header("Content-Type", "multipart/form-data")
+      .bodyPart(StringBodyPart("poslog", session => session("xmlContenu").as[String]).contentType("application/xml"))
+      .check(status.is(201)))
+    .pause(TpsPausemin seconds, TpsPausemax seconds)
 }
